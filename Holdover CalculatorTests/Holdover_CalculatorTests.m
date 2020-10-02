4af4a561c231a945c19ab508fb0fa7d40c41f20d
OnTheFly64 <gregonthefly@gmail.com>
2020-10-02 11:51:01 -0700

    Initial Commit

---
+++ Holdover CalculatorTests/Holdover_CalculatorTests.m
@@ -0,0 +1,38 @@
+//
+//  Holdover_CalculatorTests.m
+//  Holdover CalculatorTests
+//
+//  Created by Greg Lamb on 11/17/18.
+//  Copyright © 2018 Greg Lamb. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Holdover_CalculatorTests : XCTestCase
+
+@end
+
+@implementation Holdover_CalculatorTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+platform :ios, '13.0'
+workspace 'Holdover Calculator'
+
+target 'Holdover Calculator' do
+    pod 'RMStore'
+    #pod 'RMStore/RMStoreReceiptVerifier'
+    #pod 'RMStore/RMStoreTransactionReceiptVerifier'
+    #pod 'RMStore/RMStoreKeychainPersistence'
+    #pod 'RMStore/RMStoreUserDefaultsPersistence'
+    pod 'OpenSSL-Universal'
+    pod 'Bolts'
+    pod 'Parse'
+    pod 'UICKeyChainStore', '~> 2.1'
+    pod 'mailcore2-ios'
+    pod 'IQKeyboardManager'
+end
